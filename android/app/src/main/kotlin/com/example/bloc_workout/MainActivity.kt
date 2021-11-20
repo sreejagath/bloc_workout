5f330014b3224ebcac929490302f3ea6091874fa
Jagal R Nath <jagalsree007@gmail.com>
2021-11-20 14:28:15 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/bloc_workout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_workout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+//StreamBasic
+
+Stream<int> boatStream() async* {
+  for (int i = 1; i <= 10; i++) {
+    print('SENT boat no:' + i.toString());
+    await Future.delayed(Duration(seconds: 2));
+    yield i;
+  }
+}
+
+void main() async {
+  Stream<int> stream = boatStream();
+  stream.listen((data) => print('RECEIVED boat no:' + data.toString()));
+}
+
+
+//CubitBasic
