8f31b1e48a34a496aae463a393a72723cff9016a
Jagal R Nath <jagalsree007@gmail.com>
2021-11-22 08:38:43 +0530

    with bloc

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -6,7 +6,7 @@ part 'counter_state.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue: 0));
   void increment() =>
-      emit(CounterState(counterValue: state.counterValue + 1));
+      emit(CounterState(counterValue: state.counterValue + 1,isIncremented: true));
   void decrement() =>
-      emit(CounterState(counterValue: state.counterValue - 1));
+      emit(CounterState(counterValue: state.counterValue - 1,isIncremented: false));
 }
--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -2,8 +2,9 @@ part of 'counter_cubit.dart';
 
 class CounterState {
   late int counterValue;
+  late bool? isIncremented;
   CounterState({
     required this.counterValue,
+    this.isIncremented,
   });
-  
 }
